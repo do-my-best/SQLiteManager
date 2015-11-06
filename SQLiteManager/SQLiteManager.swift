a791b188b4fd58953b85b992013905232b794e12
liuzhu <1543526242@qq.com>
2015-11-07 01:39:42 +0800

    2.在单例类中添加关闭数据库的操作
    更新了函数排列格式,重要的排在最前面,次要的方法放在私有的分类中.

--- SQLiteManager/SQLiteManager.swift
+++ SQLiteManager/SQLiteManager.swift
@@ -16,6 +16,7 @@ class SQLiteManager {
     ///全局的数据库访问句柄
     private var db : COpaquePointer = nil
     
+    //MARK:  构造方法
     ///构造方法,初始化数据库信息,在创建单例对象的时候使用
     private init(){
     
@@ -23,54 +24,65 @@ class SQLiteManager {
         openDB("status.db")
     }
     
-//    #define SQLITE_INTEGER  1
-//    #define SQLITE_FLOAT    2
-//    #define SQLITE_BLOB     4
-//    #define SQLITE_NULL     5
-//    #ifdef SQLITE_TEXT
-//    # undef SQLITE_TEXT
-//    #else
-//    # define SQLITE_TEXT     3
-//    #endif
-//    #define SQLITE3_TEXT     3
+    deinit{
+        
+        sqlite3_close_v2(db)
+    }
+}
+
+//MARK: - 对数据库进行增删查改等操作
+private extension SQLiteManager{
+
     
+    //    #define SQLITE_INTEGER  1
+    //    #define SQLITE_FLOAT    2
+    //    #define SQLITE_BLOB     4
+    //    #define SQLITE_NULL     5
+    //    #ifdef SQLITE_TEXT
+    //    # undef SQLITE_TEXT
+    //    #else
+    //    # define SQLITE_TEXT     3
+    //    #endif
+    //    #define SQLITE3_TEXT     3
+    
+    //MARK: 查
     /// 执行 SQL 返回结果集合
     ///
     /// - parameter sql: sql
     func execRecord(sql:String) -> [[String:AnyObject]]? {
-    
+        
         //保存查询的结果集
         var stmt:COpaquePointer = nil
         
         //获取结果集
         if sqlite3_prepare_v2(db, sql, -1, &stmt, nil) != SQLITE_OK{
-        
+            
             print("sql 语句错误")
             return nil
         }
-
+        
         //保存结果的数组
         var selectedResult : [[String:AnyObject]] = [[String:AnyObject]]()
         
         //遍历每一条查询结果
         while sqlite3_step(stmt) == SQLITE_ROW{
-        
+            
             //保存每条查询结果的字段名和字段内容到字典
-             let dict = recordForEachField(stmt)
+            let dict = recordForEachField(stmt)
             
             //保存每条内容字典
             selectedResult.append(dict)
         }
         //释放语句句柄
         sqlite3_finalize(stmt)
-    
+        
         return selectedResult
         
     }
     
     //保存从数据库读取的一条结果的字段名和字段内容
     func recordForEachField(stmt:COpaquePointer) -> [String:AnyObject]{
-    
+        
         //定义一个字典,保存字段名和内容
         var dict : [String:AnyObject] = [String:AnyObject]();
         
@@ -117,6 +129,7 @@ class SQLiteManager {
         return dict
     }
     
+    //MARK: 改/删
     /// 执行 更新 / 删除 SQL
     ///
     /// - parameter sql: sql
@@ -132,13 +145,14 @@ class SQLiteManager {
         return Int(sqlite3_changes(db));
     }
     
+    //MARK: 增
     /// 执行插入 SQL
     ///
     /// - parameter sql: sql
     ///
     /// - returns: 返回自动增长 id
     func execInsert(sql:String)->Int{
-    
+        
         if !execSQL(sql) {
             
             return -1
@@ -148,13 +162,25 @@ class SQLiteManager {
         return Int(sqlite3_last_insert_rowid(db))
     }
     
+    //MARK: 执行 sql 的语句
+    /// 执行 SQL
+    ///
+    /// - parameter sql: SQL
+    ///
+    /// - returns: 是否成功
+    func execSQL(sql:String) -> Bool{
+        
+        return sqlite3_exec(db, sql, nil, nil, nil) == SQLITE_OK
+    }
+    
+    //MARK: 打开数据库
     ///打开数据库的操作
     ///
     /// - parameter dbName: SQL
     ///
     /// - returns: 是否成功
     func openDB(dbName:String){
-    
+        
         //document 路径
         let path = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true).last!;
         
@@ -163,7 +189,7 @@ class SQLiteManager {
         
         //打开或创建数据库
         if  sqlite3_open(filePath, &db) != SQLITE_OK{
-        
+            
             print("打开数据库失败")
             return
         }
@@ -177,30 +203,21 @@ class SQLiteManager {
         }
         
         print("打开数据表成功")
-
+        
     }
     
-    /// 执行 SQL
-    ///
-    /// - parameter sql: SQL
-    ///
-    /// - returns: 是否成功 
-    func execSQL(sql:String) -> Bool{
-    
-        return sqlite3_exec(db, sql, nil, nil, nil) == SQLITE_OK
-    }
-    
+    //MARK: 创建数据表
     /// 创建数据表,默认创建T_Person表
     ///
     /// - returns: 是否成功
     func createTable () -> Bool{
-    
+        
         //获取路径
         let filePath = NSBundle.mainBundle().pathForResource("db.sql", ofType: nil)
         
         //获取 sql 语句
         guard let sql = try? NSString(contentsOfFile: filePath!, encoding: NSUTF8StringEncoding) else{
-        
+            
             print("数据加载失败")
             return false
         }
@@ -208,4 +225,6 @@ class SQLiteManager {
         //执行 sql 语句
         return execSQL(sql as String)
     }
+    
+
 }
